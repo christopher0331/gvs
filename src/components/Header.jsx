40827a0a7ae4a8fff8d6d3d3cdec1d81dabe3135
Chris Hancock <christopher.hancock0331@gmail.com>
2021-06-09 21:31:57 -0700

    Make header component mobile friendly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Menu from './Menu.jsx';
+import './styles/Menu.css';
 
 const Header = (props) => {
 
     const Logo = "https://greenviewsolutionsimages.s3-us-west-1.amazonaws.com/HomePage/FullSizedWebp/clearGVSLogo_300.webp";
     return(
-        <div style={{backgroundColor: '#194b1e', height: '200px', width: '100%', display: 'flex', justifyContent: 'space-between'}}>
-            <img alt="this is meaningful" rel='preload' src={Logo} style={{top: '5px', left: '1px', height: '220px', width: '220px'}}/>
+        <div className="header">
+            <img className="headerLogo" alt="this is meaningful" rel='preload' src={Logo} />
             <Menu changePage={props.changePage}/>
         </div>
     )
