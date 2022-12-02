9b85cdf7998af496e54d55ebc9e2d7086bfa1847
Chris Hancock <christopher.hancock0331@gmail.com>
2022-12-01 21:59:49 -0800

    Fix header logo url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './styles/Menu.css';
 
 const Header = (props) => {
     
-    const Logo = "https://ik.imagekit.io/greenviewsolutions/HomePage/FullSizedWebp/clearGVSLogo_300.webp?tr=w-700,h-700";
+    const Logo = "https://ik.imagekit.io/greenviewsolutions/HomePage/FullSizedWebp/clearGVSLogo_300.webp?tr=w-300,h-300";
     return(
         <div className="header">
             <img className="headerLogo" rel='preload' src={Logo} alt= "Greenview Solutions Logo"/>
