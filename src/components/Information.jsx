1a973526789f65edcaedbc07e5e9bf0c29fc7c74
Chris Hancock <christopher.hancock0331@gmail.com>
2022-12-04 21:00:05 -0800

    Add AngiesList/HomeAdvisor Links/Badges

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -10,9 +10,30 @@ const Information = () => {
             <p className="paragraph">
                 GreenView Solutions provides and installs every type of fence and maintenance free railing products. 
                 From a white picket fence, to an automated gate, or an in-ground dog fence, whatever your fencing needs are, we offer a variety of products to satisfy every fencing or railing need.
+            
+            <div className="homeadvisorSeals">
+                <a href="" className="haAnchor1"> 
+                    <img alt="Rated and Reviewed HomeAdvisor Pro" className="haAnchor1" src= "http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                </a>
+                <a href="" className="haAnchor1"> 
+                    <img alt="Top Rated Contractor - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                </a>
+                <a href="" className="haAnchor1"> 
+                    <img alt="HomeAdvisor Elite Service Award - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                </a>
+                <a href="" className="haAnchor1"> 
+                    <img alt="Screened and Approved HomeAdvisor Pro" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/3year-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                </a>
+                <a href="" className="haAnchor1"> 
+                    <img alt="See Reviews at HomeAdvisor for GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/20reviews-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                </a>
+            </div>
             </p>
         </div>
     )
+
 }
+
+
 
 export default Information;
