33b0de6fdddd7c6b7312f9f5bffa33c79c46d5c2
Chris Hancock <christopher.hancock0331@gmail.com>
2022-12-05 18:13:24 -0800

    Add in American Fence Association seal

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -11,23 +11,26 @@ const Information = () => {
                 GreenView Solutions provides and installs every type of fence and maintenance free railing products. 
                 From a white picket fence, to an automated gate, or an in-ground dog fence, whatever your fencing needs are, we offer a variety of products to satisfy every fencing or railing need.
             
-            <div className="homeadvisorSeals">
-                <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
-                    <img alt="Rated and Reviewed HomeAdvisor Pro" className="haAnchor1" src= "http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
-                </a>
-                <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
-                    <img alt="Top Rated Contractor - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
-                </a>
-                <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
-                    <img alt="HomeAdvisor Elite Service Award - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
-                </a>
-                <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
-                    <img alt="Screened and Approved HomeAdvisor Pro" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/3year-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
-                </a>
-                <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
-                    <img alt="See Reviews at HomeAdvisor for GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/20reviews-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
-                </a>
-            </div>
+                <div className="homeadvisorSeals">
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
+                        <img alt="Rated and Reviewed HomeAdvisor Pro" className="haAnchor1" src= "http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                    </a>
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
+                        <img alt="Top Rated Contractor - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                    </a>
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
+                        <img alt="HomeAdvisor Elite Service Award - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                    </a>
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
+                        <img alt="Screened and Approved HomeAdvisor Pro" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/3year-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                    </a>
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
+                        <img alt="See Reviews at HomeAdvisor for GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/20reviews-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
+                    </a>
+                    <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor3"> 
+                        <img alt="See AFA at American Fence Association for GreenView Solutions" className="haAnchor3" src="https://ik.imagekit.io/greenviewsolutions/HomePage/FullSizedWebp/AFAMemberBadge2022.png?tr=w-200,h-300" />
+                    </a>
+                </div>
             </p>
         </div>
     )
