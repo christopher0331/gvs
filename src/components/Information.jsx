12f89e9d1648655d0f2c33850f78fef6db618759
Chris Hancock <christopher.hancock0331@gmail.com>
2023-11-28 14:16:00 -0800

    add in the gate badge

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -12,15 +12,22 @@ const Information = () => {
                 From a white picket fence, to an automated gate, or an in-ground dog fence, whatever your fencing needs are, we offer a variety of products to satisfy every fencing or railing need.
             
                 <div className="homeadvisorSeals">
+
                     <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor3"> 
                         <img alt="See AFA at American Fence Association for GreenView Solutions" className="haAnchor3" src="https://ik.imagekit.io/greenviewsolutions/HomePage/FullSizedWebp/AFAMemberBadge2022.png?tr=w-200,h-300" />
                     </a>
                     <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
                         <img alt="Rated and Reviewed HomeAdvisor Pro" className="haAnchor1" src= "http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
                     </a>
+                    
                     <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
                         <img alt="Top Rated Contractor - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
                     </a>
+
+                    <a href="https://www.credly.com/badges/7f1dea3a-7e84-4a35-bd64-e2ffb1e1b436/public_url" className="haAnchor1"> 
+                        <img alt="AFA Gate Auto Certification" className="haAnchor1" src="https://greenviewsolutionsimages.s3.us-west-1.amazonaws.com/op/GateInstallBadge.png" style={{height: '12em', width: '13em'}}/>
+                    </a>
+
                     <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
                         <img alt="HomeAdvisor Elite Service Award - GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
                     </a>
@@ -30,6 +37,9 @@ const Information = () => {
                     <a href="https://www.homeadvisor.com/rated.GreenViewSolutions.72536239.html" className="haAnchor1"> 
                         <img alt="See Reviews at HomeAdvisor for GreenView Solutions" className="haAnchor1" src="http://www.homeadvisor.com/images/sp-badges/20reviews-solid-border.png?sp=72536239&key=337759e38d741bd27c52f9fb13ad7b75" />
                     </a>
+                   
+                    
+
                     
                 </div>
             </p>
